73ca77b57e2e531d486e1302fa7253056393fb0d
kwanti <ngkwanti@gmail.com>
2013-12-27 19:47:55 +0800

    add zlib filter

---
+++ include/remote/bindings/zlib_filter.hpp
@@ -0,0 +1,30 @@
+// Copyright 2013 Ng Kwan Ti <ngkwanti -at- gmail.com>
+//
+// This file is distributed under GPL v2 license. You can redistribute it and/or
+// modify it under the terms of the GNU General Public License version 2 as
+// published by the Free Software Foundation. See <http://www.gnu.org/licenses/>.
+//
+// See www.cppremote.com for documentation.
+//-----------------------------------------------------------------------------
+#ifndef __REMOTE_BINDINGS_ZLIB_FILTER_HPP__
+#define __REMOTE_BINDINGS_ZLIB_FILTER_HPP__
+
+#include <remote/bindings/fwd.hpp>
+
+
+namespace remote
+{
+namespace bindings
+{
+
+class zlib_filter
+{
+public:
+	buffer_ptr output(buffer_ptr const& buf);
+	buffer_ptr input(buffer_ptr const& buf);
+};
+
+}
+}
+
+#endif
