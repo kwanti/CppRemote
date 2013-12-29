26e902bc10d7032f68fb1e0a4cff8712c9bd3dc0
kwanti <ngkwanti@gmail.com>
2013-12-29 17:12:55 +0800

    update license text

--- include/remote/make_basic_binding.hpp
+++ include/remote/make_basic_binding.hpp
@@ -1,7 +1,7 @@
 // Copyright 2013 Ng Kwan Ti <ngkwanti -at- gmail.com>
 //
-// This file is distributed under GPL v2 license. You can redistribute it and/or
-// modify it under the terms of the GNU General Public License version 2 as
+// This file is distributed under LGPL v2.1 license. You can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License version 2.1 as
 // published by the Free Software Foundation. See <http://www.gnu.org/licenses/>.
 //
 // See www.cppremote.com for documentation.
